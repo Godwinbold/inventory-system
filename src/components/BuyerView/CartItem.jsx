a0cce4a942ef3dd68d80fb2b609bc5c966e8c402
Godwinbold <gordonblaze5@gmail.com>
2024-11-23 20:51:38 +0100

    CartItem

--- src/components/BuyerView/CartItem.jsx
+++ src/components/BuyerView/CartItem.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-
+import Header from "./Header";
+import Footer from "./Footer";
 
 const CartItem = ({ item, removeFromCart }) => {
   return (
     <>
+   <Header/>
       {/* <Header /> */}
       <div className="bg-[#2E3A6E]">
         <div className="flex items-center"></div>
       </div>
+      <Footer/>
     </>
+   
   );
 };
 
